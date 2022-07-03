39cd9b19932e0b4c5920145104182b946ef9ac11
mahmud-oss <mahmud94pial@gmail.com>
2022-07-03 19:16:57 +0600

    less file added to style.less

--- carousel_button.js
+++ carousel_button.js
@@ -5,7 +5,21 @@ $(document).ready(function(){
     });
     $('#carousel-play').click(function () {
         $('#mycarousel').carousel('cycle');
-    })
+    });
+
+    // carousel control with single button
+    $('#carousel-button').click(function (){
+        if($('#carousel-button').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carousel-button').children('span').removeClass('fa-pause');
+            $('#carousel-button').children('span').addClass('fa-play');
+        }
+        else if($('#carousel-button').children('span').hasClass('fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carousel-button').children('span').removeClass('fa-play');
+            $('#carousel-button').children('span').addClass('fa-pause');
+        }
+    });
 })
 
 // $(function() {
