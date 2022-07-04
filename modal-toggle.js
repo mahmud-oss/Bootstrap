8c5af8a1676d766873855180f44caff36a2d4a22
mahmud-oss <mahmud94pial@gmail.com>
2022-07-04 19:08:10 +0600

    assignment 4

---
+++ modal-toggle.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#SignInButton').click(function(){
+        $('#signinModal').modal('toggle');
+    });
+    $('#ReserveButton').click(function(){
+        $('#reservationModal').modal('toggle');
+    })
+})
